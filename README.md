85fe4479cf61439b2aafb53bb00379610e77546d
qing762 <threatedblade@outlook.com>
2023-02-12 01:41:37 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
+> **Warning**
+> Please be advised that usage of this tool is entirely at your own risk. I assumes no responsibility for any adverse consequences that may arise from its use, and users are encouraged to exercise caution and exercise their own judgment in utilizing this tool.
+
 # ExitLag auto signup
- 
+
+A tool that auto fetch a temporary email address and creates an account at https://exitlag.com/
+
+
+
+
+## How it works
+
+The process begins by utilizing the [Temp-Mail](https://temp-mail.org/en/) service to obtain a temporary email address. This email address is then utilized to sign up for an [ExitLag](https://exitlag.com) account. Subsequently, another request is made to [Temp-Mail](https://temp-mail.org/en/) to retrieve the email confirmation link. Upon activation of the account, the user is able to log in to the ExitLag application and benefit from its functionality.
+
+
+## Features
+
+- Be able to prompt to change to your own password instead of using the default one
+- Bypass Cloudflare's bot check.
+
+> **Warning**
+> It is important to note that excessive usage of this tool may result in rate limiting by the API or, in severe cases, IP blocking. To avoid these potential consequences, it is recommended to limit the number of usage. (Maybe 5 times every 10 minutes?)
+## Installation / Usage
+
+First, clone this repository:
+```shell
+git clone https://github.com/qing762/ExitLag-auto-signup/
+```
+Install the necessary dependencies:
+```shell
+pip install -r requirements.txt
+```
+
+Next, run the Python file:
+```shell
+python main.py
+```
+
+And you're all set! Follow the instructions while interacting with the Python file.
+    
+## Contributing
+
+Contributions are always welcome!
+
+To contribute, fork this repository and improve it. After that, press the contribute button.
+## FAQ
+#### Why did you create this?
+
+Due to persistent high ping of over 100 while participating in online gaming sessions with my peers, I frequently experienced disconnections from game servers. However, after utilizing ExitLag, my ping was reduced to a more manageable level of 40+. Despite the positive outcome, I was dissatisfied with the cumbersome sign-up process. As a result, I elected to develop a custom code solution that would automate this process, also eliminating the need for monetary expenditure.
+
+Additionally, as the school holiday approached, I found myself with an abundance of free time due to a lack of pressing obligations. To combat boredom, I decided to devote my weekends to this project, which provided a productive and educational outlet for my leisure time.
+
+#### Is it bannable?
+
+I'm not sure but mostly it is as it bypasses Cloudflare's bot check.
+
+
+#### Do you plan to update it if it's patched?
+
+Unfortunately, I have no plans of updating this project for now. But you can [create an issue](https://github.com/qing762/ExitLag-auto-signup/issues/new/) if there is a problem within my code.
+
+
+#### Any plans on improving the project?
+
+Sadly, no.
+## Lessons Learned
+
+I initially perceived this task to be a formidable challenge, as circumventing Cloudflare's bot detection is known to be a complex and arduous process. However, with the aid of several dependencies, I was successful in resolving the issue. Through this experience, I was able to expand my knowledge and understanding of API requests.
+
+
+## Feedback
+
+If you have any feedback, please reach out to me at [Discord](https://discord.com/users/635765555277725696)
+## Authors
+
+- [@qing762](https://www.github.com/qing762)
+
---
+++ main.py
@@ -0,0 +1,139 @@
+import requests, httpx, cloudscraper, json, datetime, time, re
+from bs4 import BeautifulSoup
+
+client = httpx.Client(http2=True)
+
+url = "https://web2.temp-mail.org/mailbox"
+exitlagreg = f"https://www.exitlag.com/api/v2/register"
+sitekey = "9ed0f8ae-3e13-4ea8-82eb-2c112b1bc89f"
+messageurl = "https://web2.temp-mail.org/messages"
+
+print(
+'''
+M"""""`'"""`YM                dP             dP                            oo                   d88888P .d8888P d8888b. 
+M  mm.  mm.  M                88             88                                                     d8' 88'         `88
+M  MMM  MMM  M .d8888b. .d888b88 .d8888b.    88d888b. dP    dP    .d8888b. dP 88d888b. .d8888b.    d8'  88baaa. .aaadP'
+M  MMM  MMM  M 88'  `88 88'  `88 88ooood8    88'  `88 88    88    88'  `88 88 88'  `88 88'  `88   d8'   88` `88 88'
+M  MMM  MMM  M 88.  .88 88.  .88 88.  ...    88.  .88 88.  .88    88.  .88 88 88    88 88.  .88  d8'    8b. .d8 88.
+M  MMM  MMM  M `88888P8 `88888P8 `88888P'    88Y8888' `8888P88    `8888P88 dP dP    dP `8888P88 d8'     `Y888P' Y88888P
+MMMMMMMMMMMMMM                                             .88          88                  .88
+                                                       d8888P           dP              d8888P
+                                                       ''')
+time.sleep(3)
+
+key = input(f"\nPlease locate to this URL.\nhttps://hcaptcha.projecttac.com/?sitekey={sitekey}\nComplete the hCaptcha and paste in the h-captcha-response by pressing the copy button.\n")
+kk = input(f"\nInput your password for your account. Your password must be at least 6 characters. If you want to stay on the default password, ignore this and press enter:")
+
+if kk == "":
+  kk = "1234567890"
+else:
+  kk = kk
+
+scraper = cloudscraper.create_scraper(disableCloudflareV1=True, 
+    browser={
+        'browser': 'chrome',
+        'platform': 'ios',
+        'desktop': False,
+        'mobile': True
+    }
+)
+
+reque = scraper.post(url)
+print(f"\nEmail request responded with a status code of {reque.status_code}")
+dat = json.loads(reque.text)
+addr = dat["mailbox"]
+token = dat["token"]
+print(f"Fetching email address success!\n\nToken: {token}\nEmail address: {addr}\n")
+
+exitlagpayload = {
+  "email": addr,
+  "email_confirmation": addr,
+  "password": kk,
+  "password_confirmation": kk,
+  "refer_user": "0",
+  "refer_domain": "",
+  "recaptchaToken": key,
+  "locale": "en"
+}
+
+req = requests.post(exitlagreg, data=exitlagpayload)
+print(f"\nExitLag request responded with a status code of {req.status_code}")
+data = json.loads(req.text)
+tok = data["token"]
+meid = data["me"]["id"]
+banned = data["me"]["ban"]
+email_approved = data["me"]["email_approved"]
+lan = data["me"]["locale"]
+permission_provider = data["me"]["permission_provider"]
+
+print(f"ExitLag account registration has completed with the id of {meid}.\n")
+print(f"Token: {tok}\nID: {meid}\nBanned?: {banned}\nEmail approved status: {email_approved}\nLocale: {lan}\nPermission provider: {permission_provider}\n")
+
+header = {
+  "authorization": token,
+  "cache-control": "no-cache"
+}
+
+time.sleep(1)
+print("\rPlease wait.", end="")
+time.sleep(1)
+print("\rPlease wait..", end="")
+time.sleep(1)
+print("\rPlease wait...", end="")
+time.sleep(1)
+print("\rPlease wait....", end="")
+time.sleep(1)
+print("\rPlease wait.....", end="")
+time.sleep(1)
+print("\rPlease wait....", end="")
+time.sleep(1)
+print("\rPlease wait....", end="")
+time.sleep(1)
+print("\rPlease wait...", end="")
+time.sleep(1)
+print("\rPlease wait..", end="")
+time.sleep(1)
+print("\rPlease wait.", end="")
+time.sleep(1)
+print("\rPlease wait..", end="")
+time.sleep(1)
+print("\rPlease wait...", end="")
+time.sleep(1)
+print("\rPlease wait....", end="")
+time.sleep(1)
+print("\rPlease wait.....", end="")
+time.sleep(1)
+print("\rPlease wait....", end="")
+time.sleep(1)
+print("\rPlease wait...", end="")
+time.sleep(1)
+print("\rPlease wait..", end="")
+time.sleep(1)
+print("\rPlease wait.", end="")
+time.sleep(1)
+print("\r", end="")
+msg = scraper.get(messageurl, headers=header)
+msgtxt = msg.text
+da = json.loads(msgtxt)
+print(f"Fetch email data request responded with a status code of {msg.status_code}\n")
+for x in da["messages"]:
+  emailid = x["_id"]
+  recievedat = datetime.datetime.fromtimestamp(x["receivedAt"]).strftime('%Y-%m-%d %H:%M:%S')
+  fr = x["from"]
+  subject = x["subject"]
+  bodyPreview = x["bodyPreview"]
+  attachmentsCount = x["attachmentsCount"]
+print(f"Email ID: {emailid}\nRecieved at: {recievedat}\nFrom: {fr}\nSubject: {subject}\nBody Preview: {bodyPreview}\nAttachments count: {attachmentsCount}\n")
+
+exitlagverifycontent = f"https://web2.temp-mail.org/messages/{emailid}"
+ct = scraper.get(exitlagverifycontent, headers=header)
+txt = json.loads(ct.text)
+soup = BeautifulSoup(txt["bodyHtml"], 'html.parser')
+match = re.search(r"https://www.exitlag.com/verify/email/\w+", str(soup))
+
+if match:
+  link = match.group()
+  print(f"Please click this URL to verify your email address on ExitLag.\n{link}")
+  print(f"\nTo login to ExitLag with your email address created just now, use the following email address: \n{addr}\nand the following password:\n{kk}")
+else:
+  print("Link not found!")
