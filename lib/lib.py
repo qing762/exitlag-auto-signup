17bae29173a34701832b04bea9d3b4ad435eb39c
qing. <threatedblade@outlook.com>
2024-09-02 20:59:05 +0800

    Added missing module rich

--- lib/lib.py
+++ lib/lib.py
@@ -5,7 +5,7 @@ import asyncio
 import platform
 
 
-class Main():
+class Main:
     async def ainput(prompt: str = "") -> str:
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, input, prompt)
@@ -15,7 +15,9 @@ class Main():
         return "".join(random.choice(letters) for i in range(length))
 
     async def getDomain(self, request, maildomain):
-        async with request.get(f"https://api.{maildomain}/domains", params={"page": "1"}) as resp:
+        async with request.get(
+            f"https://api.{maildomain}/domains", params={"page": "1"}
+        ) as resp:
             return await resp.json()
 
     async def registerAccount(self, session, maildomain, domain, passw):
@@ -29,29 +31,30 @@ class Main():
             register = await resp.json()
         email = register["address"]
         async with session.post(
-            f"https://api.{maildomain}/token", json={"address": email, "password": passw}
+            f"https://api.{maildomain}/token",
+            json={"address": email, "password": passw},
         ) as resp:
             respJson = await resp.json()
             token = respJson["token"]
         return email, token
 
     async def switchDomain(maildomain, externaldomain):
-        print(f"Mail domain {maildomain} is not currently not available. Switching to the domain {externaldomain}...")
+        print(
+            f"Mail domain {maildomain} is not currently not available. Switching to the domain {externaldomain}..."
+        )
         return externaldomain
 
     async def getSettingsAndBlockIP(self):
         try:
             data = toml.load("settings.toml")
         except FileNotFoundError:
-            data = {
-                "blockIP": {
-                    "blocked": False
-                }
-            }
+            data = {"blockIP": {"blocked": False}}
 
         if "allowed" not in data["blockIP"]:
             while True:
-                selection = input("Due to a known issue of 0 trial days cause by the IP address *.exitlag.net, do you want to block the IP address? (y/n): ")
+                selection = input(
+                    "Due to a known issue of 0 trial days cause by the IP address *.exitlag.net, do you want to block the IP address? (y/n): "
+                )
                 if str(selection).capitalize() == "Y":
                     data["blockIP"]["allowed"] = True
                     break
@@ -61,7 +64,9 @@ class Main():
                 else:
                     print("Invalid input. Please try again.")
 
-        if data["blockIP"].get("allowed", False) and not data["blockIP"].get("blocked", False):
+        if data["blockIP"].get("allowed", False) and not data["blockIP"].get(
+            "blocked", False
+        ):
             target = ["104.22.79.205", "104.22.78.205", "172.67.29.58"]
             system = platform.system()
             if system == "Windows":
@@ -74,9 +79,13 @@ class Main():
                         hostsFile.write(f"\n127.0.0.1    {x}\n")
                         print(f"Blocked {x} in the host file.")
                 data["blockIP"]["blocked"] = True
-                print("Be sure to use a HWID spoofer as well to get the 3 days trial. One of the suggested spoofer is Monotone HWID Spoofer (https://github.com/sr2echa/Monotone-HWID-Spoofer).")
+                print(
+                    "Be sure to use a HWID spoofer as well to get the 3 days trial. Not sure this method will work currently or not, but it's worth a try."
+                )
             except PermissionError:
-                print("Permission denied. Please run the program as an administrator. This should be done once only!")
+                print(
+                    "Permission denied. Please run the program as an administrator. This should be done once only!"
+                )
                 exit(-1)
 
         with open("settings.toml", "w") as f:
